516ce0da22ce76949a04372fb738c5e5d8267582
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-04-24 20:36:52 +0200

    style: reformat using prettier

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "plugins": [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/github"
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
   ],
-  "branches": ["main"]
+  branches: ['main'],
 };
